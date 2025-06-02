7ed0d22d0f06673093938838a408e90d955c815d
Gramissxi <gramissxi@gmail.com>
2025-06-02 18:13:29 -0300

    agregue la funcion cambiarContraseña y el uso de pwinput(*)

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from utils import mostrar_menu
 from profesores import *
 
 
+
 def pre_menu():
     while True:
         print("\nMenú de Profesores")
         print("1. Mostrar Profesores")
         print("2. Ingresar")
-        print("3. Salir")
+        print("3. Cambiar contraseña")
+        print("4. Salir")
         
         opcion = input("Seleccione una opción: ").strip()
         
@@ -18,22 +20,17 @@ def pre_menu():
             mostrar_profesores()
 
         elif opcion == "2":
-            while True:
-                email = input("Ingrese su email: ").strip()
-                contraseña = input("Ingrese su contraseña: ").strip()
-                resultado = ingreso(email, contraseña)
-                if resultado:  # Si no es None, fue exitoso
-                    return True
-                break  # Sale del bucle de ingreso
-                    
-            
+            if ingreso():  
+                return True  
 
         elif opcion == "3":
+            cambiar_contraseña()
+
+        elif opcion == "4":
             print("Saliendo...")
-            break
+            return False  
         else:
             print("Opción inválida. Intente nuevamente.")
-
 
 
 
@@ -157,125 +154,3 @@ if __name__ == "__main__":
     if pre_menu() == True:
         main()
 
-from materias import *
-from asistencia import *
-from alumnos import * 
-from utils import mostrar_menu
-
-def main():
-
-    while True:
-        mostrar_menu("Sistema de Asistencia", [
-            "Gestión de Alumnos",
-            "Registrar Asistencia",
-            "Gestión de Materias",
-            "Consultar Asistencias",
-            "Salir"
-        ])
-        
-        opcion = input("Opción: ").strip()
-        
-        if opcion == "1":
-            gestion_alumnos(matriznx5)
-        elif opcion == "2":
-            registrar_asistencia(matriznx5, registro, materias)
-        elif opcion == "3":
-            gestion_materias(materias)
-        elif opcion == "4":
-            consultar_asistencias(registro, matriznx5)
-        elif opcion == "5":
-            print("¡Hasta luego!")
-            break
-        else:
-            input("Opción inválida. Presione Enter...")
-
-def gestion_alumnos(matriznx5):
-    while True:
-        mostrar_menu("Gestión de Alumnos", [
-            "Crear y cargar alumnos",
-            "Listar alumnos",
-            "Ordenar alumnos por apellido",
-            "Buscar alumno por legajo",
-            "Modificar alumno",
-            "Eliminar alumno",
-            "Volver"
-        ])
-        
-        opcion = input("Opción: ").strip()
-        
-        if opcion == "1":
-            cargar_datos()
-        elif opcion == "2":
-            mostrar_alumnos()
-        elif opcion == "3":
-            ordenar_por_apellido()
-        elif opcion == "4":
-            buscar_alumno_por_legajo()
-        elif opcion == "5":
-            modificar_alumno()
-        elif opcion == "6":
-            eliminar_alumno()
-        elif opcion == "7":
-            return
-        else:
-            input("Opción inválida. Presione Enter para continuar...")
-            
-def registrar_asistencia_menu(matriznx5, registro, materias):
-    while True:
-        mostrar_menu("Registro de Asistencia", [
-            "Cargar asistencia",
-            "Volver"
-        ])
-    
-        opcion = input("Opcion: ").strip()
-        
-        if opcion == "1":
-            registrar_asistencia(matriznx5, registro, materias)
-        elif opcion == "2":
-            return
-        else:
-            input("Opción inválida. Presione Enter para continuar...")
-
-def gestion_materias(materias):
-    while True:
-        mostrar_menu("Materias", [
-            "Listar materias",
-            "Agregar materia",
-            "Borrar materia",
-            "Volver"
-        ])
-        
-        opcion = input("Opción: ").strip()
-        
-        if opcion == "1":
-            mostrar_materias(materias)
-        elif opcion == "2":
-            agregar_materia(materias)
-        elif opcion == "3":
-            borrar_materia(materias)
-        elif opcion == "4":
-            return
-        else:
-            input("Opción inválida. Presione Enter...")
-
-def consultar_asistencias(registro, matriznx5):
-    while True:
-        mostrar_menu("Asistencias", [
-            "Por alumno",
-            "Listado general",
-            "Volver"
-        ])
-        
-        opcion = input("Opción: ").strip()
-        
-        if opcion == "1":
-            mostrar_asistencia_alumno(registro, matriznx5)
-        elif opcion == "2":
-            mostrar_asistencia_general(registro)
-        elif opcion == "3":
-            return
-        else:
-            input("Opción inválida. Presione Enter...")
-
-if __name__ == "__main__":
-    main()
--- profesores.py
+++ profesores.py
@@ -1,7 +1,10 @@
+import pwinput
+
+
 profesores = {
-    1: {"nombre": "Maria", "apellido": "Gonzalez", "email": "Maria@gmail.com", "contraseña": "Contra123", "materia": "Matematica"},
-    2: {"nombre": "Juan",  "apellido": "Perez", "email": "Juan@gmail.com", "contraseña": "Contra123", "materia": "Historia"},
-    3: {"nombre": "Luis", "apellido": "Ramirez", "email": "Luis@gmail.com", "contraseña": "Contra123", "materia": "Literatura"}
+    1: {"nombre": "Maria", "apellido": "Gonzalez", "email": "maria@gmail.com", "contraseña": "Contra123" ,"seguridad": "pepita","materia": "Matematica"},
+    2: {"nombre": "Juan",  "apellido": "Perez", "email": "juan@gmail.com", "contraseña": "Contra123","seguridad": "pepita", "materia": "Historia"},
+    3: {"nombre": "Luis", "apellido": "Ramirez", "email": "luis@gmail.com", "contraseña": "Contra123","seguridad": "pepita", "materia": "Literatura"}
 }
 
 def mostrar_profesores():
@@ -13,18 +16,66 @@ def mostrar_profesores():
     print("-" * 80)
 
 
-def ingreso(email, contraseña):
-    try:
-        for prof in profesores.values():
-            if prof["email"] == email and prof["contraseña"] == contraseña:
-                print(" ------------------------------")
-                return "Bienvenido " + prof["nombre"] + " " + prof["apellido"]
-        
-        # Si no encontró coincidencia, lanza un error manual
-        raise ValueError("Email o contraseña incorrectos. Vuelva a intentarlo.")
-    
-    except ValueError as e:
-        print(e)
-        return None  # Devuelve None si hubo error
+def ingreso():
 
+    intentos = 0
+    max_intentos = 3
+
+    while intentos < max_intentos:
+        email = input("Ingrese su email: ").strip().lower()
+        contraseña = pwinput.pwinput("Ingrese su contraseña: ", mask="*")
+
+        for prof in profesores.values():
+            if prof["email"].lower() == email and prof["contraseña"] == contraseña:
+                print(" ------------------------------")
+                print(f"Bienvenido {prof['nombre']} {prof['apellido']}")
+                return True
+
+        intentos += 1
+        print(f"Email o contraseña incorrectos. Intento {intentos} de {max_intentos}.\n")
+
+    print("Has superado el número máximo de intentos. Intenta más tarde.")
+    return False
+
+
+def cambiar_contraseña():
+    intentos_email = 0
+    max_intentos = 3
+
+    while intentos_email < max_intentos:
+        email = input("Ingrese su email: ").strip().lower()
+        profesor = None
+
+        
+        for prof in profesores.values():
+            if prof["email"] == email:
+                profesor = prof
+                break
+
+        if profesor:
+            intentos_seguridad = 0
+            while intentos_seguridad < max_intentos:
+                mascota = input("Ingrese el nombre de su mascota: ").lower()
+                if profesor.get("seguridad") == mascota:
+                    contraseña = input("Ingrese su nueva contraseña: ")
+                    conf_contraseña = input("Confirme su nueva contraseña: ")
+                    if contraseña == conf_contraseña:
+                        profesor["contraseña"] = contraseña
+                        print("Contraseña cambiada con éxito.")
+                        return True
+                    else:
+                        print("Las contraseñas no coinciden. Intente nuevamente.")
+                        return False
+                else:
+                    intentos_seguridad += 1
+                    print(f"Respuesta de seguridad incorrecta. Intento {intentos_seguridad} de {max_intentos}.\n")
+
+            print("Demasiados intentos fallidos con la respuesta de seguridad.")
+            return False
+        else:
+            intentos_email += 1
+            print(f"Email no encontrado. Intento {intentos_email} de {max_intentos}.\n")
+
+    print("Demasiados intentos fallidos con el email.")
+    return False
 
